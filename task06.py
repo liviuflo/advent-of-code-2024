ab038ce5365226ee68a976c9befb134997bbfca6
Liviu Florescu <f_liviu@yahoo.com>
2024-12-06 20:02:13 +0100

    task 6 part 2 (bruteforce hihi)

--- task06.py
+++ task06.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 import numpy as np
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 INPUT_DATA_PATH = "input_data/06.txt"
 
@@ -26,6 +27,12 @@ class Orientation(Enum):
     def get_rotated(self):
         return Orientation((self.value + 1) % 4)
 
+    def get_mark(self):
+        return self.value + 1
+
+    def clone(self):
+        return Orientation(self.value)
+
 
 START_POSITION_CHAR = "^"
 START_ORIENTATION = Orientation.up
@@ -39,35 +46,55 @@ class Position:
     def get_next_row_col(self):
         return self.row_col + np.array(self.orientation.get_delta())
 
+    def clone(self):
+        return Position(np.copy(self.row_col), self.orientation.clone())
+
 
 class MapAgent:
-    def __init__(self, input_data_path):
-        self.map, self.start_pos = read_input(input_data_path)
-        self.current_pos = self.start_pos
+    OUTSIDE = -1
+    NEW_CELL = 0
+    CROSSING = 1
+    LOOP = 2
+
+    def __init__(self, map: np.ndarray, start_pos: Position):
+        self.map = np.copy(map)
+        self.start_pos = start_pos
+        self.current_pos = start_pos.clone()
         self.cells_visited = 0
         self.mark_current_cell_as_visited()
 
     def mark_current_cell_as_visited(self):
         if not self.current_pos_is_in_grid():
-            return
+            return MapAgent.OUTSIDE
 
         row, col = self.current_pos.row_col
 
         current_value = self.map[row][col]
-        if current_value == VISITED_CELL:
-            # Already marked
-            return
+        new_value = current_value + 1
 
-        assert current_value == FREE_CELL, (row, col)
-        self.map[row][col] = VISITED_CELL
-        self.cells_visited += 1
+        self.map[row][col] = new_value
+
+        if new_value > 4:
+            return MapAgent.LOOP
+
+        if current_value > 0:
+            return MapAgent.CROSSING
+
+        if current_value == 0:
+            self.cells_visited += 1
+            return MapAgent.NEW_CELL
 
     def current_pos_is_in_grid(self):
+        return self.row_col_is_in_grid(self.current_pos.row_col)
+
+    def row_col_is_in_grid(self, row_col):
         width, height = self.map.shape
-        row, col = self.current_pos.row_col
+        row, col = row_col
         return 0 <= col < width and 0 <= row < height
 
     def cell_is_occupied(self, row_col):
+        if not self.row_col_is_in_grid(row_col):
+            return False
         row, col = row_col
         return self.map[row][col] == OCCUPIED_CELL
 
@@ -76,7 +103,7 @@ class MapAgent:
             print("Current position is not in grid.")
             return
 
-        print("Current pos:", self.current_pos)
+        # print("Current pos:", self.current_pos)
 
         next_row_col = self.current_pos.get_next_row_col()
         if self.cell_is_occupied(next_row_col):
@@ -86,8 +113,19 @@ class MapAgent:
             # move
             self.current_pos.row_col = next_row_col
 
-        self.mark_current_cell_as_visited()
-        return self.current_pos_is_in_grid()
+        return self.mark_current_cell_as_visited()
+
+    def run(self):
+        steps = []
+        while True:
+            steps.append(self.current_pos.clone())
+            # print(len(steps))
+
+            new_cell_type = self.step()
+
+            if new_cell_type in (MapAgent.OUTSIDE, MapAgent.LOOP):
+                # stop when outside or when loop has been created
+                return steps, new_cell_type
 
 
 def create_row(line: str):
@@ -118,21 +156,41 @@ def read_input(path):
     return np.array(grid), start_pos
 
 
-def part_1(agent: MapAgent):
-    while True:
-        # map_viz = np.copy(agent.map) * 100
-        # pos = agent.current_pos.row_col
-        # map_viz[pos[0]][pos[1]] = 200
-        # plt.imshow(map_viz)
-        # plt.show()
-        agent.step()
+def part_1(path: str):
+    map, start_pos = read_input(path)
+    agent = MapAgent(map, start_pos)
 
-        if not agent.current_pos_is_in_grid():
-            break
+    agent.run()
 
     print(agent.cells_visited)
 
 
+def run_is_loop(agent: MapAgent):
+    _, result = agent.run()
+    return result == MapAgent.LOOP
+
+
+def part_2(path):
+    map, start_pos = read_input(path)
+    agent = MapAgent(map, start_pos)
+
+    steps, _ = agent.run()
+    steps: list[Position]
+
+    obstacles_that_create_loops = set()
+
+    for possible_obstacle_pos in tqdm(steps[1:]):
+        new_map = np.copy(map)
+        row, col = possible_obstacle_pos.row_col
+        new_map[row][col] = OCCUPIED_CELL
+        agent = MapAgent(new_map, start_pos)
+
+        if run_is_loop(agent):
+            obstacles_that_create_loops.add((row, col))
+
+    print(len(obstacles_that_create_loops))
+
+
 if __name__ == "__main__":
-    map_agent = MapAgent(INPUT_DATA_PATH)
-    part_1(map_agent)
+    # part_1(INPUT_DATA_PATH)
+    part_2(INPUT_DATA_PATH)
