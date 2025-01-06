107ba4134950499506ea87674791e87d30acc9ea
Liviu Florescu <f_liviu@yahoo.com>
2025-01-06 14:30:37 +0200

    task 04 both parts

--- task04.py
+++ task04.py
@@ -2,46 +2,28 @@ import numpy as np
 from scipy.signal import correlate2d
 
 INPUT_DATA_PATH = "input_data/04.txt"
-# INPUT_DATA_PATH = "input_data/04_test.txt"
-# INPUT_DATA_PATH = "input_data/04_testme.txt"
 
-char_to_int = {letter: 2**i for i, letter in enumerate("XMAS")}
-char_to_int["."] = 0
-
-XMAS = [1, 2, 4, 8]
-XMAS_REV = list(reversed(XMAS))
-
-
-def line_to_list(line: str):
-    return list(map(lambda c: char_to_int[c], line.strip()))
+CHAR_TO_INT = {letter: 2**i for i, letter in enumerate("XMAS")}
+CHAR_TO_INT["."] = 0
 
 
 def read_input(path: str):
     with open(path, "r") as file:
-        lines = file.readlines()
+        return np.array(
+            [list(map(lambda c: CHAR_TO_INT[c], l.strip())) for l in file.readlines()]
+        )
 
-    return np.array([line_to_list(l) for l in lines])
 
-
-def apply_filter(matrix: np.ndarray, filter: np.ndarray):
+def apply_filter(matrix: np.ndarray, filter: np.ndarray, target_value: int):
     result = correlate2d(matrix, filter, mode="valid")
-    # print((result == 85).astype(np.int32))
-    return np.sum(result == 85)
-
-
-def pad_matrix(matrix: np.ndarray, pad_size: int):
-    top_bottom = np.zeros((pad_size, matrix.shape[1]))
-    m = np.vstack((top_bottom, matrix, top_bottom))
-
-    sides = np.zeros((m.shape[0], pad_size))
-    m = np.hstack((sides, m, sides))
-
-    return m
+    return np.sum(result == target_value)
 
 
 def part_1(path):
     matrix = read_input(path)
-    # matrix = pad_matrix(matrix, 4)
+
+    XMAS = [1, 4, 16, 64]
+    XMAS_REV = list(reversed(XMAS))
 
     filters = [
         np.array(XMAS).reshape((1, 4)),
@@ -54,9 +36,32 @@ def part_1(path):
         np.diag(XMAS_REV)[:, ::-1],
     ]
 
-    result = sum([apply_filter(matrix, f) for f in filters])
+    result = sum([apply_filter(matrix, f, target_value=585) for f in filters])
+    print(result)
+
+
+def part_2(path):
+    matrix = read_input(path)
+
+    FILTER = np.array(
+        [
+            [1, 0, 4],
+            [0, 16, 0],
+            [64, 0, 256],
+        ]
+    )
+
+    filters = [
+        FILTER,  # M top
+        FILTER.T,  # M left
+        FILTER[::-1, :],  # M bottom
+        FILTER.T[:, ::-1],  # M right
+    ]
+
+    result = sum([apply_filter(matrix, f, target_value=2634) for f in filters])
     print(result)
 
 
 if __name__ == "__main__":
-    part_1(INPUT_DATA_PATH)
+    # part_1(INPUT_DATA_PATH)
+    part_2(INPUT_DATA_PATH)
